4efd7f7d743527edb35ced1a792143d84c6cfd5a
Nankov <fn62492@g.fmi.uni-sofia.bg>
2022-11-30 10:58:54 -0800

    test commit

---
+++ settingsApp/settingsApp/settingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  settingsAppApp.swift
+//  settingsApp
+//
+//  Created by Sysprobs on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct settingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
